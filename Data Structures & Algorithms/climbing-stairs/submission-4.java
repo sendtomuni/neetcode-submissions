e2dcb0ed436b2b1e0901965f1e6bf4f58b461ea0
sendtomuni <38210565+sendtomuni@users.noreply.github.com>
2026-08-28 14:55:25 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        Integer[] dp = new Integer[n+1];
+        return help(n, dp);
+    }
+
+    public int help(int n, Integer[] dp) {
+        if(n<3) dp[n] = n;
+        if(dp[n] != null) return dp[n];
+        dp[n] = help(n-1, dp) + help(n-2, dp);
+        return dp[n];
+    }
+}
